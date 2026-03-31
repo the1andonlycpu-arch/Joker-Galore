0a69bd1a72bd2fef03261326e0fc4bac2136dc72
Robo <the1andonlycpu@gmail.com>
2026-03-31 14:16:43 -0400

    Revert "Update v1.1.0"

--- consumables/sets.lua
+++ consumables/sets.lua
@@ -6,26 +6,10 @@ SMODS.ConsumableType {
     shop_rate = 0,
     cards = {
         ['c_jokergal_payoff'] = true,
-        ['c_jokergal_interest'] = true,
-        ['c_jokergal_helper'] = true
+        ['c_jokergal_interest'] = true
     },
     loc_txt = {
         name = "Econ",
         collection = "Econ Cards",
-    }
-}
-
-SMODS.ConsumableType {
-    key = 'medival',
-    primary_colour = HEX('417505'),
-    secondary_colour = HEX('417505'),
-    collection_rows = { 4, 5 },
-    shop_rate = 0,
-    cards = {
-        ['c_jokergal_dragonegg'] = true
-    },
-    loc_txt = {
-        name = "Medival",
-        collection = "Medival Cards",
     }
 }
